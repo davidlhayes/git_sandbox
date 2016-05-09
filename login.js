ec524b443046879dff87fea3f1b846f5b326ffb2
David Hayes <davidlynnhayes@att.net>
2016-05-09 12:20:05 -0500

    change to login.js

--- login.js
+++ login.js
@@ -1,6 +1,7 @@
 function prompt(phrase) {
    var password = "uncle";
-   if (phrase==password) {
+   var key = true;
+   if (phrase==password && key) {
       alert("You are allowed access!");
    } else {
       alert("You are not allowed access!");
